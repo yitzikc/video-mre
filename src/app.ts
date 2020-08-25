b8d7cff991834e35885b4fba21571528491314d0
Yitzik Casapu <yitzikc@gmail.com>
2020-08-25 20:29:26 +0100

    Read schedule as JSON

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,10 @@
  * Licensed under the MIT License.
  */
 
+import fetch from 'node-fetch'; 
 import * as MRE from '@microsoft/mixed-reality-extension-sdk';
 import { ScheduledMediaPlayer } from './scheduled-media-player';
-
+import { getParameterLastValue } from './parameter-set-util';
 
 /**
  * The main class of this app. All the logic goes here.
@@ -16,58 +17,35 @@ export default class HelloWorld {
 	private curve: MRE.Actor = null;
 	private video: MRE.VideoStream = null;	// Remove
 	private assets: MRE.AssetContainer;
+	private mediaScheduleUrl: string;
 	private timeLine: ScheduledMediaPlayer;
 
 	constructor(private context: MRE.Context, private params: MRE.ParameterSet, private baseUrl: string) {
 		this.context.onStarted(() => this.started());
 		this.assets = new MRE.AssetContainer(this.context);
 		this.timeLine = new ScheduledMediaPlayer(this.assets, []);
-		/*
-		const value = params["vs"];
-		if (typeof(value) === 'string') {
-			this.videoUrl = decodeURIComponent(value);
-		} else if (Array.isArray(value)) {
-			this.videoUrl = decodeURIComponent(value[value.length - 1]);
-		}
-*/
+		this.mediaScheduleUrl = getParameterLastValue(params, "ms");
 	}
 
 	/**
 	 * Once the context is "started", initialize the app.
 	 */
-	private started() {
+	private async started() {
 		// eslint-disable-next-line max-len
 		// See https://github.com/microsoft/mixed-reality-extension-sdk/blob/master/packages/functional-tests/src/tests/livestream-test.ts
-		// Load a glTF model
-		//MRE.log.enable("app", "debug");
-		//MRE.log.info("app", "starting");
 
 		this.curve = MRE.Actor.CreatePrimitive(this.assets, {
 			definition: { shape: MRE.PrimitiveShape.Box, dimensions: { z:1 } }
 		});
 
-		const mediaSchedule = this.loadMediaSchedule();
+		const mediaSchedule = await this.loadMediaSchedule();
 		this.timeLine = new ScheduledMediaPlayer(this.assets, mediaSchedule);
 		this.timeLine.start(this.curve);
 	}
 
 	// Stub implementation. Replace with actual implementation
-	private loadMediaSchedule = () => {
-		return [ {
-			startTime: "+00:00:5",
-			endTime: "+00:00:20",
-			uri: "youtube://wHDVX2E8nLY",
-			volume: 1,
-		}, {
-			startTime: "+00:00:30",
-			uri: "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4?_=1",
-			volume: 1,
-		}, {
-			startTime: "+00:00:45",
-			uri: "https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8",
-			endTime: "+00:00:20",
-			volume: 1,
-		}
-		];
+	private async loadMediaSchedule() {
+		const schedule = await fetch(this.mediaScheduleUrl).then(res => res.json());
+		return schedule;
 	}
 }
---
+++ src/parameter-set-util.ts
@@ -0,0 +1,37 @@
+import { ParameterSet, Color3, Color4, } from '@microsoft/mixed-reality-extension-sdk';
+
+// Get the last of potentially multiple values of a parameter in an MRE parameter set
+export function getParameterLastValue(params: ParameterSet, name: string, dflValue: string = ''): string {
+	const value = params[name];
+	if (typeof(value) === 'string') {
+		return value;
+	} else if (Array.isArray(value)) {
+		return value[value.length - 1];
+	}
+
+	return dflValue;
+}
+
+// Get the value of a boolean parameter whose value can be 'y' or 'n'
+export function getBooleanOption(params: ParameterSet, name: string, dfl = false): boolean {
+	const assumeIfNotGiven = dfl ? 'y' : 'n'
+	return (getParameterLastValue(params, name, assumeIfNotGiven)[0].toLowerCase() == 'y');
+}
+
+// Get a 3 or 4-channel colour from an option
+export function getColorOption(
+	params: ParameterSet, name: string, dfl: Color3|Color4
+): Color3|Color4 {
+	const colorSpec = "#" + getParameterLastValue(params, name, "").replace("#", " ").trim();
+	switch (colorSpec.length) {
+		case 7:
+			return Color3.FromHexString(colorSpec);
+		case 9:
+			return Color4.FromHexString(colorSpec);
+		case 1:
+			return dfl;
+		default:
+			// FIXME: Throw a specific error class.
+			throw Error(`Invalid HEX color: ${colorSpec}`);
+	}
+}
